4f7247081b8a6879a6d6a54b6ab7e0ea91dd141f
kiranraj98 <kiranraj98@outlook.com>
2024-09-10 22:55:16 +0800

    making changes

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "google.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
